1486691923e75c20e9c2f090a15fbcfa3a734d1b
Masatoshi Kubode <masatoshi@wantedly.com>
2020-10-07 22:25:11 +0900

    init2

---
+++ Podfile
@@ -0,0 +1,10 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'playground-renovate-cocoapods' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for playground-renovate-cocoapods
+  pod "Then", "2.0.0"
+end
---
+++ playground-renovate-cocoapods/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  playground-renovate-cocoapods
+//
+//  Created by Masatoshi Kubode on 2020/10/07.
+//  Copyright © 2020 Wantedly, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
